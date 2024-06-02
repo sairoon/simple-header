7377aca7882fb3a3e9714129534414a19ab6a925
Mahmud Sairoon <smbdmahmud1010@gmail.com>
2024-06-03 01:29:43 +0600

    first commit

---
+++ js/style.js
@@ -0,0 +1,24 @@
+/*
+function wave() {
+    var wave = document.querySelector(".wave");
+    var width = wave.offsetWidth;
+    var height = wave.offsetHeight;
+    var x = 0;
+    var y = 0;
+
+    var animation = setInterval(function() {
+        x += 1;
+        y += 1;
+        wave.style.transform = "translate(" + x + "px, " + y + "px)";
+
+        if (x > width) {
+            x = 0;
+        }
+
+        if (y > height) {
+            y = 0;
+        }
+    }, 10);
+}
+
+window.addEventListener("load", wave);*/
